fec558d869de6627362d0893d53e81dff7ffc590
Paulo <ph46163835@gmail.com>
2023-03-06 17:20:12 -0300

    feat: add uma buscando cozinha por id no banco

---
+++ src/main/java/com/yourfood/restaurant/jpa/BuscarCozinhaMain.java
@@ -0,0 +1,23 @@
+package com.yourfood.restaurant.jpa;
+
+import com.yourfood.restaurant.RestaurantApplication;
+import com.yourfood.restaurant.domain.model.Cozinha;
+import org.springframework.boot.WebApplicationType;
+import org.springframework.boot.builder.SpringApplicationBuilder;
+import org.springframework.context.ApplicationContext;
+
+public class BuscarCozinhaMain {
+
+    public static void main(String[] args) {
+        ApplicationContext applicationContext = new SpringApplicationBuilder(
+                RestaurantApplication.class)
+                .web(WebApplicationType.NONE)
+                .run(args);
+
+        CadastroCozinha cadastroCozinha = applicationContext.getBean(CadastroCozinha.class);
+
+        Cozinha cozinha = cadastroCozinha.buscar(2L);
+
+         System.out.println(cozinha.getNome());
+    }
+}
--- src/main/java/com/yourfood/restaurant/jpa/CadastroCozinha.java
+++ src/main/java/com/yourfood/restaurant/jpa/CadastroCozinha.java
@@ -17,6 +17,10 @@ public class CadastroCozinha {
                 .getResultList();
     }
 
+    public  Cozinha buscar(Long id) {
+        return manager.find(Cozinha.class, id);
+    }
+
     @Transactional
     public Cozinha adicionar(Cozinha cozinha) {
         return manager.merge(cozinha);
