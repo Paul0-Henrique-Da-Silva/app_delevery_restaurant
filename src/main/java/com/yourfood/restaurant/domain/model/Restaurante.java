64891abbec7f68895b0833519bbb7d9b2df7de77
Paulo <ph46163835@gmail.com>
2023-03-06 18:17:43 -0300

    feat: model  restaurante 'ManytoOne' p/ cozinha e config dialeto innoDb

--- src/main/java/com/yourfood/restaurant/domain/model/Restaurante.java
+++ src/main/java/com/yourfood/restaurant/domain/model/Restaurante.java
@@ -3,11 +3,7 @@ package com.yourfood.restaurant.domain.model;
 import lombok.Data;
 import lombok.EqualsAndHashCode;
 import java.math.BigDecimal;
-import javax.persistence.Column;
-import javax.persistence.Entity;
-import javax.persistence.GeneratedValue;
-import javax.persistence.GenerationType;
-import javax.persistence.Id;
+import javax.persistence.*;
 
 @Data
 @EqualsAndHashCode(onlyExplicitlyIncluded = true)
@@ -24,4 +20,6 @@ public class Restaurante {
 	@Column(name = "taxa_frete")
 	private BigDecimal taxaFrete;
 
+	@ManyToOne
+	private Cozinha cozinha;
 }
